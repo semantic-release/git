2a57c5b81875ff326e43c01922bc540e451daa85
babblebey <babblebey@gmail.com>
2026-03-03 19:28:17 +0100

    refactor: standardize code formatting and improve readability across multiple files

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-import { defaultTo, castArray } from 'lodash-es';
-import verifyGit from './lib/verify.js';
-import prepareGit from './lib/prepare.js';
+import { defaultTo, castArray } from "lodash-es";
+import verifyGit from "./lib/verify.js";
+import prepareGit from "./lib/prepare.js";
 
 let verified;
 
 export function verifyConditions(pluginConfig, context) {
-  const {options} = context;
+  const { options } = context;
   // If the Git prepare plugin is used and has `assets` or `message` configured, validate them now in order to prevent any release if the configuration is wrong
   if (options.prepare) {
     const preparePlugin =
-      castArray(options.prepare).find((config) => config.path && config.path === '@semantic-release/git') || {};
+      castArray(options.prepare).find(
+        (config) => config.path && config.path === "@semantic-release/git",
+      ) || {};
 
     pluginConfig.assets = defaultTo(pluginConfig.assets, preparePlugin.assets);
-    pluginConfig.message = defaultTo(pluginConfig.message, preparePlugin.message);
+    pluginConfig.message = defaultTo(
+      pluginConfig.message,
+      preparePlugin.message,
+    );
   }
 
   verifyGit(pluginConfig);
